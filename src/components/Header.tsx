0f792e0df572698b01bc5be379208f36c4147816
Abdulrahman <rahman3000@mail.ru>
2023-09-21 15:50:42 +0300

    fix: починил корзину

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import Cart from "./Cart";
 import img1 from "./img/logo.jpg";
 import img2 from "./img/user.svg";
-import cart2 from './img/cart2.png'
+import cart2 from "./img/cart2.png";
 import style from "./Header/header.module.css";
 import { RootState } from "../app/store";
 function Header() {
@@ -34,22 +34,29 @@ function Header() {
           {" "}
           <span>Конфигуратор ПК</span>
         </Link>
-        <Link to="assort"> <span>Комплектующие</span></Link>
+        <Link to="assort">
+          {" "}
+          <span>Комплектующие</span>
+        </Link>
         <Link to="/">
           {" "}
           <span>Главная</span>
         </Link>
       </div>
 
-      
+      <div className={style.cartIcon}>
         <div className={style.cartIcon} onClick={toggleCart}>
-          <button className={style.cart}
-            onClick={() => { <div >{isCartVisible && <Cart />}</div> }
-            }>
-            <img src={cart2} /></button>
+          <button
+            className={style.cart}
+            onClick={() => {
+              <div>{isCartVisible && <Cart />}</div>;
+            }}
+          >
+            <img src={cart2} />
+          </button>
           {/* Cart ({cartItemsCount}) */}
-        
-        {isCartVisible && <Cart />}
+          {isCartVisible && <Cart />}
+        </div>
         {getTok ? (
           <Link to="logroom">
             <img src={img2} alt="" />
