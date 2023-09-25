3f234b1a82b087c271e25ed0e67dba9a212f11b1
Abdulrahman <rahman3000@mail.ru>
2023-09-25 21:28:24 +0300

    feat:сделал 3д сборщик моделей

--- src/components/Assembling.tsx
+++ src/components/Assembling.tsx
@@ -2,73 +2,107 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { fetchCategory } from "../features/CategorySlice";
 import { CarouselHome } from "./cssComponent/Carousel";
+import { Suspense } from "react";
+
 import style from "../css/assembling.module.css";
-import { fetchAccessories, fetchAccessoriesCategory, fetchOneAccessories } from "../features/AccessoriesSlice";
+import {
+  fetchAccessories,
+  fetchAccessoriesCategory,
+  fetchOneAccessories,
+} from "../features/AccessoriesSlice";
 import { AppDispatch } from "../app/store";
+import Test from "../components/threeJsComponents/test";
+
 import One from "./One";
 
 function Assembling() {
+  const handleClickThreeJs = () => {
+    setThreeJs(!threejs);
+  };
   const dispatch = useDispatch<AppDispatch>();
   const category = useSelector((state) => state.categorySlice.category);
-  const accessories = useSelector((state) => state.accessoriesSlice.accessories)
-  const oneAcces =  useSelector((state)=> state.accessoriesSlice.oneAccessori)
-  
-
-
+  const accessories = useSelector(
+    (state) => state.accessoriesSlice.accessories
+  );
+  const [threejs, setThreeJs] = useState(false);
+  const oneAcces = useSelector((state) => state.accessoriesSlice.oneAccessori);
+  const modelslink = useSelector(
+    (state) => state.accessoriesSlice.oneAccessori
+  );
+  console.log(modelslink);
+  console.log(category);
 
   useEffect(() => {
     dispatch(fetchCategory());
     dispatch(fetchAccessories());
-}, [dispatch]);
+  }, [dispatch]);
 
-  const handleClick = (id)=>{
-    dispatch(fetchAccessoriesCategory(id))
+  const handleClick = (id) => {
+    dispatch(fetchAccessoriesCategory(id));
+  };
 
-  }
-  const handleOpenClick = (id, acces)=>{ 
-    const inArray = oneAcces.filter((item) => item.category === acces.category)
-    
-    
-    
+  const handleOpenClick = (id, acces) => {
+    const inArray = oneAcces.filter((item) => item.category === acces.category);
+
     if (inArray.length < 1) {
-      dispatch(fetchOneAccessories(id))  
+      dispatch(fetchOneAccessories(id));
     }
-  }
-  
-  
-  
-  return (
-       <> <CarouselHome/>
-    <div className={style.rodblock_assem}>
+  };
 
-      <div>
-        <div className={style.compl}>
-          <span>Комплектация</span>
-          
+  return (
+    <>
+      {" "}
+      <CarouselHome />
+      <div className={style.rodblock_assem}>
+        <div>
+          <div className={style.compl}>
+            <span>Комплектация</span>
+          </div>
+          {category?.map((item) => {
+            return (
+              <div
+                onClick={() => handleClick(item._id)}
+                className={style.title}
+              >
+                <div>
+                  {" "}
+                  <span>{item.title}</span>
+                </div>
+
+                {oneAcces.map((ac) => {
+                  if (item._id === ac.category) {
+                    return (
+                      <div className={style.tit_img}>
+                        <div className={style.image_acces}>
+                          <img src={ac.image} alt="" />
+                        </div>
+                        <div className={style.title_acces}>{ac.title}</div>
+                      </div>
+                    );
+                  }
+                })}
+              </div>
+            );
+          })}
         </div>
 
-        {category?.map((item) => {
-          return (
-            <div onClick={()=>handleClick(item._id)} className={style.title}>
-               <div> <span>{item.title}</span></div>
-              {oneAcces.map((ac)=>{
-                if(item._id === ac.category){
-                  return(
-                    <div className={style.tit_img}>
-                      <div className={style.image_acces}><img src={ac.image} alt="" /></div>
-                      <div className={style.title_acces}>{ac.title}</div>
-                    </div>
-                  )
-                }
-              })}
-            </div>
-          );
-        })}
+        {threejs ? (
+          <div className={style.testDiv}>
+            <Suspense fallback={<div>Loading...</div>}>
+              {threejs && <Test modelslink={modelslink} category={category} />}
+            </Suspense>
+          </div>
+        ) : (
+          <div className={style.twoRod}>
+            {accessories?.map((acces) => (
+              <One handleOpenClick={handleOpenClick} acces={acces} />
+            ))}
+          </div>
+        )}
+        <div className={style.switchThreeJs}>
+          <button onClick={handleClickThreeJs}>показать в 3д</button>
+        </div>
       </div>
-      <div className={style.twoRod}>
-        {accessories?.map((acces)=> <One handleOpenClick={handleOpenClick} acces={acces}/>)}
-      </div>
-    </div>
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import Cart from "./Cart";
 import img1 from "./img/logo.jpg";
 import img2 from "./img/user.svg";
-import cart2 from './img/cart2.png'
+import cart2 from "./img/cart2.png";
 import style from "./Header/header.module.css";
 import { RootState } from "../app/store";
 function Header() {
@@ -28,30 +28,38 @@ function Header() {
       <div className={style.logo}>
         <img src={img1} alt="" />
       </div>
+
       <div className={style.three_span}>
-      <Link to="sborka"> <span>Компьютеры</span></Link> 
-        <Link to="assem">
-          {" "}
-          <span>Конфигуратор ПК</span>
-        </Link>
-        <Link to="assort"> <span>Комплектующие</span></Link>
         <Link to="/">
           {" "}
           <span>Главная</span>
         </Link>
+        <Link to="sborka">
+          {" "}
+          <span>Компьютеры</span>
+        </Link>
+        <Link to="assem">
+          {" "}
+          <span>Конфигуратор ПК</span>
+        </Link>
+        <Link to="assort">
+          {" "}
+          <span>Комплектующие</span>
+        </Link>
       </div>
 
-      
-        <div className={style.cartIcon} >
-          <div onClick={toggleCart}>
+      <div className={style.cartIcon}>
+        <div onClick={toggleCart}>
+          <button
+            className={style.cart}
+            onClick={() => {
+              <div>{isCartVisible && <Cart />}</div>;
+            }}
+          >
+            <img src={cart2} />
+          </button>
+        </div>
 
-          <button className={style.cart}
-            onClick={() => { <div >{isCartVisible && <Cart />}</div> }
-          }>
-            <img src={cart2} /></button>
-          
-              </div>
-        
         {isCartVisible && <Cart />}
         {getTok ? (
           <Link to="logroom">
